aab38e910e3ee10da892cdda2a7b0aab96162c9b
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2026-01-22 23:09:39 +0200

    adjust margin

--- src/routes/(storefront)/+page.svelte
+++ src/routes/(storefront)/+page.svelte
@@ -20,7 +20,7 @@
 
 <div>
   <!-- Hero Section -->
-  <section class="bg-gray-100 pt-16 pb-20">
+  <section class="bg-gray-100 pt-10 pb-20">
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div class="flex items-center gap-20">
         <div>
