d4e36a12482319ac8aef5483ebb8e75d3ae987fc
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2026-01-23 03:52:54 +0200

    change button bg

--- src/routes/(storefront)/+page.svelte
+++ src/routes/(storefront)/+page.svelte
@@ -47,7 +47,7 @@
             <img src="/kuvitus2.png" alt="Svelte" class="h-20 w-20" />
             <a
               href="/products"
-              class="inline-block rounded-lg border bg-white px-8 py-3 font-semibold text-gray-900 transition-colors hover:bg-gray-100"
+              class="h inline-block rounded-lg border bg-white px-8 py-3 font-semibold text-gray-900 transition-colors hover:bg-[#f7d0dd]/50"
             >
               Get Started: Docs
             </a>
