446a5f7991b853c81af0f6822402d34a3c010ecc
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2026-01-24 21:17:39 +0200

    sonner styles

--- src/lib/components/admin/ui/sonner/sonner.svelte
+++ src/lib/components/admin/ui/sonner/sonner.svelte
@@ -9,12 +9,14 @@
   toastOptions={{
     classes: {
       toast:
-        "group toast group-[.toaster]:bg-white group-[.toaster]:text-gray-950 group-[.toaster]:border-gray-200 group-[.toaster]:shadow-lg",
+        "group toast group-[.toaster]:bg-white group-[.toaster]:text-gray-950 group-[.toaster]:border-gray-200 group-[.toaster]:shadow-lg group-[.toaster]:rounded-lg",
       description: "group-[.toast]:text-gray-500",
       actionButton: "group-[.toast]:bg-gray-900 group-[.toast]:text-gray-50",
       cancelButton: "group-[.toast]:bg-gray-100 group-[.toast]:text-gray-500",
-      error: "group-[.toaster]:bg-red-50 group-[.toaster]:text-red-600 group-[.toaster]:border-red-200",
-      success: "group-[.toaster]:bg-green-50 group-[.toaster]:text-green-600 group-[.toaster]:border-green-200"
+      error:
+        "group-[.toaster]:!bg-red-50 group-[.toaster]:!text-red-700 group-[.toaster]:!border-red-300 [&>svg]:!text-red-500",
+      success:
+        "group-[.toaster]:!bg-green-50 group-[.toaster]:!text-green-700 group-[.toaster]:!border-green-300 [&>svg]:!text-green-500"
     }
   }}
   {...props}
