d50ef6107e5c28bb32f2bdc395892c1c16cc56b1
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2026-02-07 13:08:26 +0200

    edit hero

--- src/routes/(storefront)/+page.svelte
+++ src/routes/(storefront)/+page.svelte
@@ -22,7 +22,7 @@
 
 <div>
   <!-- Hero Section -->
-  <section class="bg-gray-100 pt-10 pb-20">
+  <section class="bg-gray-100 pt-10 pb-16">
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div class="flex items-center gap-16">
         <div>
