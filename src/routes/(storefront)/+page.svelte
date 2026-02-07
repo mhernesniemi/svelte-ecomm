f3e8718b63faa35dcbaa147bc2286222e3a218a8
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2026-02-07 16:59:23 +0200

    update hero text

--- src/routes/(storefront)/+page.svelte
+++ src/routes/(storefront)/+page.svelte
@@ -32,7 +32,7 @@
           <h1 class="mb-4 text-4xl font-bold md:text-5xl">Opinionated Commerce</h1>
           <p class="mb-10 text-lg leading-[1.75] text-gray-600">
             Lightweight but powerful e-commerce gear, designed for developers.<br />
-            It's 100% customizable and owned by you.
+            Everything lives in code and it’s yours.
             <span class="text-gray-900 italic">Built with SvelteKit.</span>
           </p>
           <div class="flex items-start gap-4">
